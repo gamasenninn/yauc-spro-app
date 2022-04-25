95f25611347bfc51b7ac6c1526faac6e582e953a
gamasenninn <gamasenninn@yahoo.co.jp>
2022-04-25 10:58:24 +0900

    gitignore

--- app/csv2db_order.py
+++ app/csv2db_order.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv()
 download_dir = os.environ['DOWNLOAD_DIR']
 order_filename = os.environ['ORDER_FILENAME']
 
-csv_path = os.path.join(download_dir,order_filename)
+csv_path = os.path.join(download_dir, order_filename)
 df = pd.read_csv(csv_path, encoding='cp932')
 
+engine = create_engine('sqlite:///storpro.db', echo=False)
+df.to_sql("order", engine, if_exists='replace')
+
+print("updated DB....OK")
