d192672d65019884adb1457c528775124b1e1088
gamasenninn <gamasenninn@yahoo.co.jp>
2022-04-25 10:58:34 +0900

    退避

--- app/csv2db_order.py
+++ app/csv2db_order.py
@@ -6,11 +6,12 @@ from sqlalchemy import create_engine
 load_dotenv()
 download_dir = os.environ['DOWNLOAD_DIR']
 order_filename = os.environ['ORDER_FILENAME']
+db_name = os.environ['SQLITE_DB_NAME']
 
 csv_path = os.path.join(download_dir, order_filename)
 df = pd.read_csv(csv_path, encoding='cp932')
 
-engine = create_engine('sqlite:///storpro.db', echo=False)
+engine = create_engine(f'sqlite:///{db_name}.db', echo=False)
 df.to_sql("order", engine, if_exists='replace')
 
 print("updated DB....OK")
