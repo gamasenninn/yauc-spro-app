69404e25f865de511ded11e1aff6ef91c5d15a59
gamasenninn <gamasenninn@yahoo.co.jp>
2022-06-12 15:50:23 +0900

    paramiko test

--- app/test_paramiko.py
+++ app/test_paramiko.py
@@ -9,25 +9,31 @@ SSH_USERNAME =  os.environ['SSH_USERNAME']
 SSH_PASSWORD =  os.environ['SSH_PASSWORD']
 
 
-
-with paramiko.SSHClient() as ssh:
-    # 初回ログイン時に「Are you sure you want to continue connecting (yes/no)?」と
-    # きかれても問題なく接続できるように。
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    # ssh接続
-    ssh.connect(SSH_HOST, port=SSH_PORT, username=SSH_USERNAME, password=SSH_PASSWORD)
-
-    # コマンド実行
-    stdin, stdout, stderr = ssh.exec_command('ls -al')
-    stdin, stdout, stderr = ssh.exec_command('ls -al web/close')
-
-    # コマンド実行後に標準入力が必要な場合
-    # stdin.write('password\n')
-    # stdin.flush()
-
-    # 実行結果を表示
+def print_stdout_stderr(stdout,stderr):
     for o in stdout:
         print('[std]', o, end='')
     for e in stderr:
         print('[err]', e, end='')
+
+with paramiko.SSHClient() as ssh:
+    # Are you sure you want to continue connecting (yes/no)?
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # ssh接続
+    ssh.connect(SSH_HOST, port=SSH_PORT, username=SSH_USERNAME, password=SSH_PASSWORD,timeout=10.0)
+
+    # コマンド実行
+    stdin, stdout, stderr = ssh.exec_command('ls -al web/close/back')
+    print_stdout_stderr(stdout,stderr)
+
+    try:
+        sftp = ssh.open_sftp()
+        sftp.put('c:/IMAGE/SCODE/15002-1.jpg','web/close/back/15002-1.jpg')
+        for f in sftp.listdir('web/close/back'):
+            print(f)
+    finally:
+        sftp.close()
+
+    # コマンド実行後に標準入力が必要な場合
+    # stdin.write('password\n')
+    # stdin.flush()
