5aa6a15313a228b948d40a55a2c932f5a8cf3bbc
gamasenninn <gamasenninn@yahoo.co.jp>
2022-09-24 13:09:36 +0900

    PREF追加コンテナ起動

---
+++ app/pref_restart_container.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import subprocess
+from dotenv import load_dotenv
+
+import prefect
+from prefect import task, Flow
+from prefect.run_configs import LocalRun
+
+from restart_container import restart_container
+
+dir_name = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv()
+restart_container_name = os.environ['RESTART_CONTAINER_NAME'].split(',')
+
+@task
+def t_restart_container(t):
+    #---- copy analize data ----- 
+    for container_name in restart_container_name:
+        if restart_container(container_name):
+            print(f"{container_name} container restarted !")
+        else:
+            print(f"{container_name} Ignored")
+    return True
+
+@task
+def t_end(t):
+    #---- copy Image data ----- 
+    print("restart container task end")
+    return True
+
+with Flow("restart_container",run_config=LocalRun(working_dir=dir_name)) as flow:
+    #init_driver()
+    t1 = t_restart_container('')
+    t2 = t_end(t1)
+
+
+if __name__ == '__main__':
+
+    args = sys.argv
+
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    print(dir_name)
+
+    #flow.run_config = LocalRun(working_dir=dir_name)
+    if len(args) >1 :
+        if args[1] == 'reg':
+            flow.register(project_name="test")
+    else:
+        flow.run()
+    pass
--- app/restart_container.py
+++ app/restart_container.py
@@ -1,12 +1,39 @@
 import docker
-client = docker.from_env()
+from dotenv import load_dotenv
+import os
 
-for image in client.images.list():
-    print (image.id,image.tags)
+def print_container_list(client):
+    print("----- Container ------")
+    for container in client.containers.list():
+        print (container.id,container.name,)
 
-for container in client.containers.list():
-    print (container.id,container.name,)
 
-container = container = client.containers.get('selchrome')
-container.stop()
-container.start()
+def print_image_list(client):
+    print("----- Image ------")
+    for image in client.images.list():
+        print (image.id,image.tags)
+
+def restart_container(container_name):
+    client = docker.from_env()
+
+    container = container = client.containers.get(container_name)
+    container.stop()
+    container.start()
+
+    return True
+
+if __name__ == '__main__':
+
+    load_dotenv()
+    restart_container_name = os.environ['RESTART_CONTAINER_NAME'].split(',')
+
+    client = docker.from_env()
+
+    print_image_list(client)
+    print_container_list(client)
+
+    for container_name in restart_container_name:
+        if restart_container(container_name):
+            print(f"{container_name} container restarted !")
+        else:
+            print(f"{container_name} Ignored")
